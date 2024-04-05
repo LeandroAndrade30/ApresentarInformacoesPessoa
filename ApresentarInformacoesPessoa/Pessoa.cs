a6e0373a193e05ef02dc6599c2870c71fc655023
Leandro Andrade <leandrade666@gmail.com>
2024-04-04 21:38:40 -0300

    Criada a classe Pessoa com seus atributos e o método apresentar

---
+++ ApresentarInformacoesPessoa/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApresentarInformacoesPessoa
+{
+    public  class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+
+        public Pessoa(string nome, int idade, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+
+        public  void Apresentar()
+
+        {
+            Console.WriteLine($"O nome da pessoa é: {Nome}");
+            Console.WriteLine($"A Idade da pessoa é: {Idade}");
+            Console.WriteLine($"O Altura da pessoa é: {Altura}");
+        }
+    }
+}
---
+++ ApresentarInformacoesPessoa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ApresentarInformacoesPessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome da pessoa");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a idade  da pessoa");
+            int  idade = int.Parse( Console.ReadLine());
+            Console.WriteLine("Informe a altura  da pessoa");
+            double altura = double.Parse(Console.ReadLine());
+            Pessoa p = new Pessoa(nome,idade,altura);
+            p.Apresentar();
+        }
+    }
+}
